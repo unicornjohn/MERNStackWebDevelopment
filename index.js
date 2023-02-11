bca1da915fadd47ae3f0ec5b28ce574a0eadc3e2
Souradeep Ghosh <116977697+unicornjohn@users.noreply.github.com>
2023-02-11 20:53:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+// another file create
+// console.log('Namaste Dunia version4');
+// let a = true;
+
+
+// console.log(a)
+// const num =12;
+// let lastname = 'babbar';
+// console.log(lastname);
+
+// lastname=3;
+// console.log(lastname);
+// console.log(3 ===3);
+
+// let age =27;
+// let status = (age >=18)? 'Yes Vote' : 'No Vote';
+
+// console.log(status);
+
+// let marks = 12;
+// if(marks >=90){
+//     console.log('A');
+// }
+// else if (marks > = 80){
+//     console.log('B');
+// }
+//  else if (marks >=70){
+//     console.log('C');
+
+// }
+// else {
+//     console.log('E');
+// }
+
+// let num = 2;
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: conmsole.log('C');
+//     break;
+//     default: console.log('E');
+// }
+
+// for(let i=0 ;i<5 ; i=i+1)
+// {
+//     console.log(i)
+// }
+// let i=0;
+// while(i<10){
+//     console.log(i)
+//     i++;
+// }
+let i=1;
+do{
+    console.log(i)
+    i++;
+}
+while(i<10);
